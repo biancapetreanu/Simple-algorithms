12054ec9ce94bd10aa1974cb660384b0d776529b
biancapetreanu <52544118+biancapetreanu@users.noreply.github.com>
2024-04-13 00:33:44 +0300

    Inserted link to my Upwork profile
    Inserted link to my Upwork profile cause writing code on my computer and then putting it on the web didn't get me any money so I have to find other ways to make money

--- program 6.py
+++ program 6.py
@@ -2,3 +2,5 @@ str="*"
 for i in range(0,10):
     str1 = str(i-1)+str1+str(i-1)
     print(str1)
+
+//If you found this program useful and need to do a similar task for you hire me on Upwork, here's the link:https://www.upwork.com/freelancers/~01b84a07429dc1171a?mp_source=share
